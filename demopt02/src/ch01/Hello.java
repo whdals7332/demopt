8eea2ba41e50c43b09329e1865a7e5f32f279f3c
user <user@192.168.219.184>
2021-10-01 17:57:23 +0900

    git 학습 테스트

--- demopt02/src/ch01/Hello.java
+++ demopt02/src/ch01/Hello.java
@@ -1,9 +1,9 @@
 package ch01;
 
 public class Hello {
-	
+
 	public static void main(String[] args) {
-		System.out.println("git 공부!!");
-		System.out.println("git 공부!! 2");
+		
+		System.out.println("git학습");
 	}
 }
